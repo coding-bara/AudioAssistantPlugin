aaf57d339ddef972fb5701b8a5cdb19b2e5f6744
Max Rost <mx.rst@gmx.net>
2023-12-24 13:00:19 +0100

    chore: minor refactoring

--- src/AudioAssistantPlugin/Device/AudioDeviceActive.cs
+++ src/AudioAssistantPlugin/Device/AudioDeviceActive.cs
@@ -25,21 +25,22 @@
       }
     }
 
-    public BitmapImage GetDialIcon() => EmbeddedResources.ReadImage(
-      Instance.State.IsMuted
-        ? $"{nameof(Loupedeck)}.{nameof(AudioAssistantPlugin)}.Resources.Icons.Size50x50.{_type}.Muted.png"
-        : $"{nameof(Loupedeck)}.{nameof(AudioAssistantPlugin)}.Resources.Icons.Size50x50.{_type}.Normal.png"
-    );
-
-    public BitmapImage GetButtonIcon(String state = default) {
-      String resourceName;
-
-      if (state != default)
-        resourceName = $"{nameof(Loupedeck)}.{nameof(AudioAssistantPlugin)}.Resources.Icons.Size80x80.{_type}.{Instance.Type}.{state}.png";
-      else
-        resourceName = $"{nameof(Loupedeck)}.{nameof(AudioAssistantPlugin)}.Resources.Icons.Size80x80.{_type}.{Instance.Type}.png";
+    public BitmapImage GetDialIcon() {
+      var resourceName = Instance.State.IsMuted
+        ? $"{GetIconBasePath(50)}.{_type}.Muted.png"
+        : $"{GetIconBasePath(50)}.{_type}.Normal.png";
 
       return EmbeddedResources.ReadImage(resourceName);
     }
+
+    public BitmapImage GetButtonIcon(String state = default) {
+      var resourceName = state != default
+        ? $"{GetIconBasePath(80)}.{_type}.{Instance.Type}.{state}.png"
+        : $"{GetIconBasePath(80)}.{_type}.{Instance.Type}.png";
+
+      return EmbeddedResources.ReadImage(resourceName);
+    }
+
+    private String GetIconBasePath(Int32 size) => $"{nameof(Loupedeck)}.{nameof(AudioAssistantPlugin)}.Resources.Icons.Size{size}x{size}";
   }
 }
